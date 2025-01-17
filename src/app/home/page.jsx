860369ee24b4f665b7f8ae415c1ad68607e3acaf
Eventlistener <chanfcsg334@gmail.com>
2025-01-17 17:46:17 +0700

    final project

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -5,10 +5,9 @@ import NewsCard from "@/components/newCard";
 
 // Fetch News Data
 async function fetchNewsData() {
-  const api_key = process.env.NEXT_PUBLIC_API_KEY;
   try {
     const response = await axios.get(
-      `https://newsapi.org/v2/top-headlines?country=us&pageSize=12&apiKey=${api_key}`
+      `https://newsapi.org/v2/top-headlines?country=us&pageSize=12&apiKey=8c783b7ea7844d02bf898cb39b48f84a`
     );
     const articles = response.data.articles.filter(article => article.urlToImage);
 
